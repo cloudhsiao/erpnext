9041cd9da957ec635621c5b8a2948ee073510647
cloud <cloudhs@gmail.com>
2017-02-18 00:36:58 +0800

    remove setting up for production

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt-get update && apt-get install -y wget ca-certificates sudo cron supervisor
 /usr/bin/supervisord
 
 wget https://raw.githubusercontent.com/frappe/bench/master/install_scripts/setup_frappe.sh
-bash setup_frappe.sh --setup-production
+bash setup_frappe.sh
 rm /home/frappe/*.deb
 
 exit 0
